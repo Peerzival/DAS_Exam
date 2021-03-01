b11ef5021f7ecd86c7a810d760f159d581f7b407
derReloader <62664722+derReloader@users.noreply.github.com>
2021-03-01 16:30:18 +0100

    Bugfixing ArticleMicroService (organize imports: package name change)

--- ArticleMicroservice/src/main/java/de/leuphana/component/structure/Article.java
+++ ArticleMicroservice/src/main/java/de/leuphana/component/structure/Article.java
@@ -1,16 +1,9 @@
-package de.leuphana.component.article.structure;
+package de.leuphana.component.structure;
 
-import java.util.Map;
-
-import javax.persistence.CollectionTable;
-import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.MapKeyColumn;
 
 @Entity
 public class Article {
